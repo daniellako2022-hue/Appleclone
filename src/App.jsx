9aa796322dd5f0f942f78d2e7f9bccc95ff13c52
daniellako0 <daniellako2022@gmail.com>
2026-07-01 21:56:06 +0300

    Apple

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Header from "./components/Header/Header";
+import Alert from "./components/Alert Section/Alert";
+import FirstSection from "./components/First section/FirstSection";
+import SecondSection from "./components/Second section/SecondSection";
+import ThirdSection from "./components/project/ThirdSection";
+import FourthSection from "./components/project/FourthSection";
+import FifthSection from "./components/project/FifthSection";
+import SixSection from "./components/project/sixSection";
+import YoutubeVideos from "./components/project/YoutubeVideos";
+import Footer from "./components/project/Footer";
+
+
+function App() {
+  return <div> 
+  <Header /> 
+  <Alert />
+  <FirstSection />
+  <SecondSection />
+  <ThirdSection />
+  <FourthSection />
+  <FifthSection />
+  <SixSection />
+  < YoutubeVideos />
+  <Footer />
+  </div>
+  
+}
+
+  
+export default App;
---
+++ src/components/project/Api.js
@@ -0,0 +1,318 @@
+{
+"kind"= "youtube#searchListResponse",
+"etag"= "HzEVjDJGoOlAjEGeo7iQHlz8KvI",
+"nextPageToken"= "CAkQAA",
+"regionCode"= "ET",
+"pageInfo"= {
+"totalResults":217,
+"resultsPerPage": 9
+},
+"items"= [
+{
+"kind": "youtube#searchResult",
+"etag": "JRzGASt5up-aKtdF1I9JO-x_97o",
+"id": {
+"kind": "youtube#video",
+"videoId": "_fI--z7jPzQ"
+},
+"snippet": {
+"publishedAt": "2026-03-04T14:23:21Z",
+"channelId": "UCE_M8A5yxnLfW0KghEeajjw",
+"title": "MacBook Neo in Citrus #Shorts",
+"description": "",
+"thumbnails": {
+"default": {
+"url": "https://i.ytimg.com/vi/_fI--z7jPzQ/default.jpg",
+"width": 120,
+"height": 90
+},
+"medium": {
+"url": "https://i.ytimg.com/vi/_fI--z7jPzQ/mqdefault.jpg",
+"width": 320,
+"height": 180
+},
+"high": {
+"url": "https://i.ytimg.com/vi/_fI--z7jPzQ/hqdefault.jpg",
+"width": 480,
+"height": 360
+}
+},
+"channelTitle": "Apple",
+"liveBroadcastContent": "none",
+"publishTime": "2026-03-04T14:23:21Z"
+}
+},
+{
+"kind": "youtube#searchResult",
+"etag": "3WIZfSFPd9cl02Fp05bjiHq7xg0",
+"id": {
+"kind": "youtube#video",
+"videoId": "3Eou6-_3Vzg"
+},
+"snippet": {
+"publishedAt": "2025-11-27T17:00:43Z",
+"channelId": "UCE_M8A5yxnLfW0KghEeajjw",
+"title": "Apple Holiday: Bring on the Holidays with Dual Capture on iPhone 17 Pro",
+"description": "This holiday, remember that friendship is a gift. Squirrel wanted to give the world a little peak of what set life is like on our latest ...",
+"thumbnails": {
+"default": {
+"url": "https://i.ytimg.com/vi/3Eou6-_3Vzg/default.jpg",
+"width": 120,
+"height": 90
+},
+"medium": {
+"url": "https://i.ytimg.com/vi/3Eou6-_3Vzg/mqdefault.jpg",
+"width": 320,
+"height": 180
+},
+"high": {
+"url": "https://i.ytimg.com/vi/3Eou6-_3Vzg/hqdefault.jpg",
+"width": 480,
+"height": 360
+}
+},
+"channelTitle": "Apple",
+"liveBroadcastContent": "none",
+"publishTime": "2025-11-27T17:00:43Z"
+}
+},
+{
+"kind": "youtube#searchResult",
+"etag": "pLXe3MKhjdDWSUQuBynt4TWjwxc",
+"id": {
+"kind": "youtube#video",
+"videoId": "xv9w4zBULlg"
+},
+"snippet": {
+"publishedAt": "2025-12-26T19:30:26Z",
+"channelId": "UCE_M8A5yxnLfW0KghEeajjw",
+"title": "Watch: Made To Keep You Moving",
+"description": "Apple Watch keeps track of your workouts, helping motivate you to keep moving despite the temptation of quitting. Young Franco ...",
+"thumbnails": {
+"default": {
+"url": "https://i.ytimg.com/vi/xv9w4zBULlg/default.jpg",
+"width": 120,
+"height": 90
+},
+"medium": {
+"url": "https://i.ytimg.com/vi/xv9w4zBULlg/mqdefault.jpg",
+"width": 320,
+"height": 180
+},
+"high": {
+"url": "https://i.ytimg.com/vi/xv9w4zBULlg/hqdefault.jpg",
+"width": 480,
+"height": 360
+}
+},
+"channelTitle": "Apple",
+"liveBroadcastContent": "none",
+"publishTime": "2025-12-26T19:30:26Z"
+}
+},
+{
+"kind": "youtube#searchResult",
+"etag": "MOy2Dh8Yef2ki7lktPav1aMuTSM",
+"id": {
+"kind": "youtube#video",
+"videoId": "WJxDLtYwVsk"
+},
+"snippet": {
+"publishedAt": "2026-03-13T16:02:05Z",
+"channelId": "UCE_M8A5yxnLfW0KghEeajjw",
+"title": "Meet iPhone 17e. A whole lot of storage. For a lot less. #shorts",
+"description": "The new iPhone 17e comes with 256GB of starting storage. Which means you'll have more space for what matters — from ...",
+"thumbnails": {
+"default": {
+"url": "https://i.ytimg.com/vi/WJxDLtYwVsk/default.jpg",
+"width": 120,
+"height": 90
+},
+"medium": {
+"url": "https://i.ytimg.com/vi/WJxDLtYwVsk/mqdefault.jpg",
+"width": 320,
+"height": 180
+},
+"high": {
+"url": "https://i.ytimg.com/vi/WJxDLtYwVsk/hqdefault.jpg",
+"width": 480,
+"height": 360
+}
+},
+"channelTitle": "Apple",
+"liveBroadcastContent": "none",
+"publishTime": "2026-03-13T16:02:05Z"
+}
+},
+{
+"kind": "youtube#searchResult",
+"etag": "z_NzzgKZ9rynWLKlfomOMnbsnEo",
+"id": {
+"kind": "youtube#video",
+"videoId": "NqLVKqsjxf8"
+},
+"snippet": {
+"publishedAt": "2025-12-26T19:30:36Z",
+"channelId": "UCE_M8A5yxnLfW0KghEeajjw",
+"title": "Watch: Made To Keep You Moving",
+"description": "Apple Watch keeps track of your workouts, helping motivate you to keep moving despite the temptation of quitting. Young Franco ...",
+"thumbnails": {
+"default": {
+"url": "https://i.ytimg.com/vi/NqLVKqsjxf8/default.jpg",
+"width": 120,
+"height": 90
+},
+"medium": {
+"url": "https://i.ytimg.com/vi/NqLVKqsjxf8/mqdefault.jpg",
+"width": 320,
+"height": 180
+},
+"high": {
+"url": "https://i.ytimg.com/vi/NqLVKqsjxf8/hqdefault.jpg",
+"width": 480,
+"height": 360
+}
+},
+"channelTitle": "Apple",
+"liveBroadcastContent": "none",
+"publishTime": "2025-12-26T19:30:36Z"
+}
+},
+{
+"kind": "youtube#searchResult",
+"etag": "o6NqD0JoJXaOJW8FQ9sQdntqq00",
+"id": {
+"kind": "youtube#video",
+"videoId": "EKB8_OeWGDo"
+},
+"snippet": {
+"publishedAt": "2023-12-20T17:00:15Z",
+"channelId": "UCE_M8A5yxnLfW0KghEeajjw",
+"title": "How to pay in store with Apple Pay using iPhone. #Shorts",
+"description": "Learn more on http://apple.com/apple-pay Audio Descriptions: https://youtube.com/shorts/5MuTZ-l8V7U #ApplePay.",
+"thumbnails": {
+"default": {
+"url": "https://i.ytimg.com/vi/EKB8_OeWGDo/default.jpg",
+"width": 120,
+"height": 90
+},
+"medium": {
+"url": "https://i.ytimg.com/vi/EKB8_OeWGDo/mqdefault.jpg",
+"width": 320,
+"height": 180
+},
+"high": {
+"url": "https://i.ytimg.com/vi/EKB8_OeWGDo/hqdefault.jpg",
+"width": 480,
+"height": 360
+}
+},
+"channelTitle": "Apple",
+"liveBroadcastContent": "none",
+"publishTime": "2023-12-20T17:00:15Z"
+}
+},
+{
+"kind": "youtube#searchResult",
+"etag": "vxlBthFNyIPwmDfbwrtJxo6jVGI",
+"id": {
+"kind": "youtube#video",
+"videoId": "ChA6jxuUU3s"
+},
+"snippet": {
+"publishedAt": "2025-06-09T18:51:18Z",
+"channelId": "UCE_M8A5yxnLfW0KghEeajjw",
+"title": "Big things coming to iPad. #Shorts",
+"description": "This fall, you'll be able to multitask with a ton of windows, access files from your Dock, navigate with a menu bar, and do so much ...",
+"thumbnails": {
+"default": {
+"url": "https://i.ytimg.com/vi/ChA6jxuUU3s/default.jpg",
+"width": 120,
+"height": 90
+},
+"medium": {
+"url": "https://i.ytimg.com/vi/ChA6jxuUU3s/mqdefault.jpg",
+"width": 320,
+"height": 180
+},
+"high": {
+"url": "https://i.ytimg.com/vi/ChA6jxuUU3s/hqdefault.jpg",
+"width": 480,
+"height": 360
+}
+},
+"channelTitle": "Apple",
+"liveBroadcastContent": "none",
+"publishTime": "2025-06-09T18:51:18Z"
+}
+},
+{
+"kind": "youtube#searchResult",
+"etag": "oPux9funE9qwdW-Ev65FZF8ljj4",
+"id": {
+"kind": "youtube#video",
+"videoId": "xqtA8gfWENc"
+},
+"snippet": {
+"publishedAt": "2025-11-27T17:00:59Z",
+"channelId": "UCE_M8A5yxnLfW0KghEeajjw",
+"title": "Apple Holiday: Capture the Cheer on iPhone 17 Pro",
+"description": "This holiday, remember that friendship is a gift. Watch the full Apple holiday film on our Youtube channel where handmade ...",
+"thumbnails": {
+"default": {
+"url": "https://i.ytimg.com/vi/xqtA8gfWENc/default.jpg",
+"width": 120,
+"height": 90
+},
+"medium": {
+"url": "https://i.ytimg.com/vi/xqtA8gfWENc/mqdefault.jpg",
+"width": 320,
+"height": 180
+},
+"high": {
+"url": "https://i.ytimg.com/vi/xqtA8gfWENc/hqdefault.jpg",
+"width": 480,
+"height": 360
+}
+},
+"channelTitle": "Apple",
+"liveBroadcastContent": "none",
+"publishTime": "2025-11-27T17:00:59Z"
+}
+},
+{
+"kind": "youtube#searchResult",
+"etag": "psirGneGKzAF3g1Z5KA73Quk_2c",
+"id": {
+"kind": "youtube#video",
+"videoId": "e7uq_8Eo_Oo"
+},
+"snippet": {
+"publishedAt": "2025-09-24T19:21:43Z",
+"channelId": "UCE_M8A5yxnLfW0KghEeajjw",
+"title": "Let Loose: Featuring 8x Zoom on iPhone 17 Pro",
+"description": "Jackson Wang on the set of “Let Loose” with 8x optical-quality zoom on iPhone 17 Pro. Commissioned by Apple.",
+"thumbnails": {
+"default": {
+"url": "https://i.ytimg.com/vi/e7uq_8Eo_Oo/default.jpg",
+"width": 120,
+"height": 90
+},
+"medium": {
+"url": "https://i.ytimg.com/vi/e7uq_8Eo_Oo/mqdefault.jpg",
+"width": 320,
+"height": 180
+},
+"high": {
+"url": "https://i.ytimg.com/vi/e7uq_8Eo_Oo/hqdefault.jpg",
+"width": 480,
+"height": 360
+}
+},
+"channelTitle": "Apple",
+"liveBroadcastContent": "none",
+"publishTime": "2025-09-24T19:21:43Z"
+}
+}
+]
+}
